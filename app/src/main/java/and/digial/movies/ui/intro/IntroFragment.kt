ee4beaf7228871601775633590fa0c27df96d347
Tony Owen <tony.owen@and.digital>
2021-11-25 11:41:46 +0000

    Search fragment, and navigation to

--- app/src/main/java/and/digial/movies/ui/intro/IntroFragment.kt
+++ app/src/main/java/and/digial/movies/ui/intro/IntroFragment.kt
@@ -6,8 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.button.MaterialButton
 
 class IntroFragment: Fragment() {
+
+    lateinit var startButton: MaterialButton
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,7 +19,15 @@ class IntroFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_intro, container, false)
+        startButton = view.findViewById(R.id.intro_start_button)
         return view
+    }
+
+    override fun onStart() {
+        super.onStart()
+        startButton.setOnClickListener {
+            findNavController().navigate(IntroFragmentDirections.introToSearch())
+        }
     }
 
 }
---
+++ app/src/main/java/and/digial/movies/ui/search/SearchFragment.kt
@@ -0,0 +1,20 @@
+package and.digial.movies.ui.search
+
+import and.digial.movies.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class SearchFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_search, container, false)
+        return view
+    }
+}
